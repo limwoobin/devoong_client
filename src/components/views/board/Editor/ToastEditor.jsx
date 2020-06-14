3953d046bbda056ae718cf9f3cc64510457265cc
drogba <drogba02@naver.com>
2020-06-14 15:45:54 +0900

    Toast Editor hooks 변경 , 코드블록 language 추가

--- src/components/views/board/Editor/ToastEditor.jsx
+++ src/components/views/board/Editor/ToastEditor.jsx
@@ -1,57 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState , useEffect } from 'react';
 import Editor from '@toast-ui/editor';
-
-import 'tui-color-picker/dist/tui-color-picker.min';
-import 'tui-editor/dist/tui-editor-extColorSyntax';
-import 'codemirror/lib/codemirror.css';
-import 'tui-editor/dist/tui-editor.css';
-import 'tui-editor/dist/tui-editor-contents.css';
-import 'highlight.js/styles/github.css';
-import 'tui-color-picker/dist/tui-color-picker.min.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import java from 'highlight.js/lib/languages/java';
+import python from 'highlight.js/lib/languages/python';
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('python', python);
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/i18n/ko-kr';
 import './ToastEditor.css';
 
 let toastEditor;
-class ToastEditor extends Component {
-    constructor(){
-        super();
-        this.state = {
-            content : ''
-        };
-    };
 
-    componentDidMount = () => {
+const hookEditor = () => {
+    
+    const [content , setContent] = useState('');
+
+    useEffect(() => {
         toastEditor = new Editor({
             el: document.querySelector('#editSection'),
             initialEditType: 'wysiwyg', // 'markdown'
             previewStyle: 'vertical',
             height: '600px',
-            exts: ['colorSyntax']
+            exts: ['colorSyntax'],
+            plugins: [[codeSyntaxHighlight , { hljs }]],
+            language: 'ko-KR'
         });
-    };
+    } , []);
 
-    saveArticle = () => {
+    const saveArticle = () => {
         const content = toastEditor.getHtml();
         console.log(content)
-
-        this.setState({
-            content
-        });
+        setContent(content);
     };
 
-    render(){
-        return (
-            <div id="toastEditor">
-                <h1>Toast UI Editor Example</h1>
-                <div id="editSection"></div>
-                <button onClick={this.saveArticle} className="btn_save">Save</button>
-                <div>
-                    <h2>result</h2>
-                    <textarea className="tf_result" value={this.state.content} readOnly="readOnly"></textarea>
-                </div>
+    return (
+        <div id="toastEditor">
+            <h1>Toast UI Editor Example</h1>
+            <div id="editSection"></div>
+            <button onClick={saveArticle} className="btn_save">Save</button>
+            <div>
+                <h2>result</h2>
+                <textarea className="tf_result" value={content} readOnly="readOnly"></textarea>
             </div>
-        );
-    };
+        </div>
+    )
+}
 
-};
-
-export default ToastEditor;
+export default hookEditor;
