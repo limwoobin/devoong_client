4560b30378a59a01954d6a390b782f996c3ee254
drogba <drogba02@naver.com>
2020-08-04 20:50:30 +0900

    404 page 수정

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -6,7 +6,7 @@ const NoMatch : React.FC = () => {
   return (
     <div>
       <div className="iRWwWQ">
-        <img src="../../public/NotFound.png" style={{ marginTop: '10%' }} />
+        <img src="https://www.gaia-wind.com/images/404.png" style={{ marginTop: '10%' }} />
         <Link to="/" style={{ textDecoration: 'none' }}>
             <button color="teal" className="homeBtn">홈으로</button>
         </Link>
