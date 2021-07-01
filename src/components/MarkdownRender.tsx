b815204a0b346210be219327982ff05062d7be11
limwoobin <drogba02@naver.com>
2021-07-01 20:04:49 +0900

    add - markdown 렌더러 도입

--- src/components/MarkdownRender.tsx
+++ src/components/MarkdownRender.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import ReactMarkdown from 'react-markdown';
+import './markdown.scss';
 
-const MarkdownStyle = styled.div`
+const MarkDownStyle = styled.div`
     font-size: 1rem;
     line-height: 2.5rem;
     h1 {
@@ -10,32 +11,15 @@ const MarkdownStyle = styled.div`
     }
 `;
 
-const markdown = `
-# zzz
-## zz222
-### asdasdasd
-일반 텍스트
-**굵게**
-\`\`\`
-    코드블락
-\`\`\`
-
-*기울이기*
-
-글자 \`배경색\`
-
-> 인용문
->> 이건뭘까
-`;
 
 export default function MarkdownRender(props: any) {
     const { contents } = props;
     // const markdownContents = ;
 
     return (
-        <MarkdownStyle>
-            {/* <ReactMarkdown>{markdown}</ReactMarkdown> */}
-            <ReactMarkdown>{contents}</ReactMarkdown>
-        </MarkdownStyle>
+        // <MarkDownStyle>
+            <ReactMarkdown
+            children={contents} />
+        // </MarkDownStyle>
     )
 }
--- src/components/views/posts/PostsView.tsx
+++ src/components/views/posts/PostsView.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import MarkdownRender from '../../MarkdownRender';
 
 const contents = `
-    # H1 ~
-    ## H2 zz
-    ### H312e313rr
+# H1 ~
+## H2 zz
+### H312e313rr
 `;
 
 export default function PostsView() {
