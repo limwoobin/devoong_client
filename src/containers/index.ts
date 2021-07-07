9733e9bebee46a80bbe8f0a15bd491adffad2078
limwoobin <drogba02@naver.com>
2021-07-07 20:13:11 +0900

    delete - 미사용 container 삭제

--- src/containers/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PostsContainer } from './PostsContainer';
-export { default as TagsContainer } from './TagsContainer';
-export { default as RecentPostsContainer } from './RecentPostsContainer';
