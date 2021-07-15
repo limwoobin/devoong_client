7385a6bf9b3e125890b1c34e6efe3589f9f9a75f
limwoobin <drogba02@naver.com>
2021-07-15 21:14:10 +0900

    add - rootReducer 에 tags 추가

--- src/reducer/rootReducer.ts
+++ src/reducer/rootReducer.ts
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 import postsReducer , { postsSaga } from './postsReducer';
+import tagsReducer , {tagsSaga} from './tagsReducer';
 import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
-    postsReducer
+    postsReducer,
+    tagsReducer
 });
 
 export function* rootSaga() {
-    yield all([postsSaga()]);
+    yield all([postsSaga() , tagsSaga()]);
 }
 
 export default rootReducer;
