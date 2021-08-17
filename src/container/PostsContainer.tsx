5acd41d8bfbff7ebf96177720f45fdb70786be4c
limwoobin <drogba02@naver.com>
2021-08-17 20:52:50 +0900

    refactor - paging 화면접근성 개선 #2

--- src/container/PostsContainer.tsx
+++ src/container/PostsContainer.tsx
@@ -1,10 +1,12 @@
-import React , { useEffect } from 'react';
+import React , { useState , useEffect } from 'react';
 import { useDispatch , useSelector } from 'react-redux';
 import PostsList from '../views/posts/PostsList';
 import { searchPostsAsync , initLoadingState } from '../reducer/postsReducer';
 import { RootState } from '../reducer/rootReducer';
 import Paging from '../views/common/Paging';
 import { Pageable } from '../models';
+import qs from 'qs';
+import { createBrowserHistory } from 'history';
 
 function onSearchPosts(dispatch: any , pageable: Pageable) {
 	dispatch(searchPostsAsync(pageable));
@@ -22,12 +24,34 @@ interface IPostsContainer {
 export default function PostsContainer({ tagId , searchWord }: IPostsContainer) {
 	const dispatch = useDispatch();
 	
+	const [pageNumber , setPageNumber] = useState(1);
+
+	const history = createBrowserHistory();
+
 	function onPageChange(e: any , page: number) {
-		const pageNumber: number = page - 1;
-		onSearchPosts(dispatch , new Pageable(pageNumber));
+		setPageNumber(page);
+		onSearch();
+	}
+
+	function onSearch() {
+		console.log('pageNumber' , pageNumber);
+		history.push(`/posts?page=${pageNumber}`);
+		onSearchPosts(dispatch , new Pageable(pageNumber - 1));
 	}
 
 	useEffect(() => {
+		const filterParams = history.location.search.substr(1);
+		const filtersFromParams = qs.parse(filterParams);
+
+		console.log('filtersFromParams' , filtersFromParams);
+		console.log('filtersFromParams.page' , filtersFromParams.page);
+
+		if (filtersFromParams.page) {
+			setPageNumber(Number(filtersFromParams.page));
+			onPageChange(null , Number(filtersFromParams.page));
+			return;
+		}
+
 		if (posts.content === undefined) {
 			onInitLoadingState(dispatch);
 			onSearchPosts(dispatch , new Pageable(0));
@@ -48,6 +72,7 @@ export default function PostsContainer({ tagId , searchWord }: IPostsContainer) 
 	return (
 		<>
 			<PostsList data={posts} isLoading={isLoading} />
+			{pageNumber}
 			<Paging 
 				totalPages={posts.totalPages} 
 				pagable={posts.pageable}
