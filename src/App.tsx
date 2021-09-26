4ae843821b01fc6778578a33f5bbd4aaba52b7e2
limwoobin <drogba02@naver.com>
2021-09-26 19:23:09 +0900

    add - SEO component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Header from '@/views/layout/header/Header';
 import './styles/style.scss';
 import Main from '@/views/Main';
 import Container from '@material-ui/core/Container';
+import SEO from './SEO';
 
 export default function App() {
 	return (
 		<div style={{backgroundColor: '#363636'}}>
 			<Router>
+				<SEO />
 				<Header />
 				<Container maxWidth="lg">
 					<div style={{paddingBottom: '20px' , minHeight: '100%'}}>
