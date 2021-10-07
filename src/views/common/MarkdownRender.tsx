5036c052f879a6ef0dce7f86189905d49f536216
limwoobin <drogba02@naver.com>
2021-10-07 19:49:48 +0900

    modify - markdown template css 수정 (code block , css file)

--- src/views/common/MarkdownRender.tsx
+++ src/views/common/MarkdownRender.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Progress from './Progress';
-// import './markdown.scss';
-import './testMarkdown.css';
+import './markdown-template.css';
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import rehypeRaw from 'rehype-raw';
@@ -17,28 +16,27 @@ export default function MarkdownRender({data , isLoading}: IMarkdownRender) {
 	return (
 		<div style={{ color: 'white' , paddingTop: '10px'}}>
 			{!isLoading ? <Progress /> : 
-			<article className="markdown-body-dark">
-				<ReactMarkdown 
-					children={data}
-					remarkPlugins={[[remarkGfm , { singleTilde: false }]]}
-					rehypePlugins={[rehypeRaw]}
-					components={{
-						code({ className , children })  {
-							const language = className?.replace("language-", "");
-							return (
-									<SyntaxHighlighter
-										style={darcula}
-										language={language}
-										children={children[0]}
-										showLineNumbers={true}
-									/>
-							);
-						},
-					}}
-				/>
-					{/* {data}
-				</ReactMarkdown> */}
-			</article>
+				<article className="markdown-body-dark">
+					<ReactMarkdown 
+						children={data}
+						remarkPlugins={[[remarkGfm , { singleTilde: false }]]}
+						rehypePlugins={[rehypeRaw]}
+						components={{
+							code({ className , children })  {
+								const language = className?.replace("language-", "");
+								return (
+										<SyntaxHighlighter
+											class="devoong-cd-block"
+											style={darcula}
+											language={language}
+											children={children[0]}
+											showLineNumbers={true}
+										/>
+								);
+							},
+						}}
+					/>
+				</article>
 			}
 		</div>
 	);
