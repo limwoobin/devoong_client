bcf3660c663235c595e116c49434f3b03dda014b
limwoobin <drogba02@naver.com>
2021-10-07 20:41:10 +0900

    add - ReactMarkdown remark lib 추가 및 적용

--- src/views/common/MarkdownRender.tsx
+++ src/views/common/MarkdownRender.tsx
@@ -6,6 +6,8 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
+import remarkToc from 'remark-toc';
+import remarkSlug from 'remark-slug';
 
 interface IMarkdownRender {
 	data: string;
@@ -19,14 +21,15 @@ export default function MarkdownRender({data , isLoading}: IMarkdownRender) {
 				<article className="markdown-body-dark">
 					<ReactMarkdown 
 						children={data}
-						remarkPlugins={[[remarkGfm , { singleTilde: false }]]}
+						// remarkPlugins={[[remarkGfm , { singleTilde: false }]]}
+						remarkPlugins={[remarkGfm , remarkSlug , remarkToc]}
 						rehypePlugins={[rehypeRaw]}
 						components={{
 							code({ className , children })  {
 								const language = className?.replace("language-", "");
 								return (
 										<SyntaxHighlighter
-											class="devoong-cd-block"
+											className="devoong-cd-block"
 											style={darcula}
 											language={language}
 											children={children[0]}
