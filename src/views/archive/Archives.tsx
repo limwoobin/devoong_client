9487559163da5ef45673191824f124fd9ee6cbed
limwoobin <drogba02@naver.com>
2021-12-10 19:54:46 +0900

    refactor - archive css 수정

--- src/views/archive/Archives.tsx
+++ src/views/archive/Archives.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Timeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
@@ -14,6 +13,8 @@ import { Link } from 'react-router-dom';
 import { TitleView } from '@/views/common';
 import ArchiveBox from './ArchivesBox';
 import Container from '@material-ui/core/Container';
+import { useMediaQuery } from 'react-responsive';
+import { constants } from '@/core/constant';
 
 interface IArchives {
 	data: ArchiveGroupModel[];
@@ -21,11 +22,13 @@ interface IArchives {
 }
 
 function renderArchivesGroup(data: ArchiveGroupModel[]) {
+	const isMobile = useMediaQuery({query: constants.MAX_WIDTH});
+
 	if (data !== undefined) {
 		return data.map((archive: ArchiveGroupModel , index: number) => (
 			<TimelineItem key={index}>
 				<TimelineOppositeContent>
-					<Typography variant="h6" component="h1" style={{ fontWeight: 'bold' , fontFamily: 'BMJUA' }}>
+					<Typography variant="h6" component="h1" style={{ fontWeight: 'bold' , fontFamily: 'BMJUA' , fontSize: isMobile ? '1rem' : '1.5em' }}>
 						{archive.createdYear} ({archive.archives.length})
 					</Typography>
 				</TimelineOppositeContent>
@@ -43,9 +46,15 @@ function renderArchivesGroup(data: ArchiveGroupModel[]) {
 
 
 function renderArchives(data: ArchiveModel[]) {
+	const style = {
+		color: 'white',
+		textDecoration: 'none',
+		width: '100%'
+	};
+
 	if (data !== undefined) {
 		return data.map((archive: ArchiveModel , index: number) => (
-			<Link key={index} to={`/posts/${archive.id}`} style={{ color: 'white' , textDecoration: 'none' }}>
+			<Link key={index} to={`/posts/${archive.id}`} style={style}>
 				<ArchiveBox key={index} title={ `${archive.title} (${archive.createdDate})` } />
 			</Link>
 		));
--- src/views/archive/ArchivesBox.tsx
+++ src/views/archive/ArchivesBox.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { useMediaQuery } from 'react-responsive';
+import { constants } from '@/core/constant';
 
 const useStyles = makeStyles(() => 
 	createStyles({
 		root: {
 			backgroundColor: '#858585',
 			fontWeight: 'bold',
-			fontSize: '1rem',
 			color: 'white',
 			marginBottom: '8px',
 			marginTop: '5px',
@@ -21,13 +22,14 @@ interface IArchiveBox {
 }
 
 export default function ArchiveBox(props: IArchiveBox) {
+	const isMobile = useMediaQuery({query: constants.MAX_WIDTH});
 	const classes = useStyles();
 	const { title } = props;
 
 	return (
 		<>
 			<Box className={classes.root} p={1}>
-				<div style={{ marginLeft: '20px' }}>
+				<div style={{ marginLeft: '20px' , fontSize: isMobile ? '0.7rem' : '1rem'}}>
 					{title}
 				</div>
 			</Box>
